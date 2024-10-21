74055f7e73e4857ad881c77325c30cb6058ef102
Takk <takkxiiv@gmail.com>
2024-10-21 17:12:25 +0800

    commit-Cart

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -10,30 +10,29 @@ function HomeHeader() {
     const switchPop = () => setPopSwitch(!popSwitch)
 
     return (
-        <>
-    <header>
-        <div className="ptabh gennavBar removeborder cenVer">
-            {/* <!-- 底線陰影 --> */}
-            <div className="fixedspace paLR20 cenVer arrHor horizontallySpaceBetween">
-                <img className="h45" src={LOGO} alt="LOGO" />
-                <ul className="noBullets arrHor noBreaks titleText">
-                    <li className="texthover rightline padLR35">首頁</li>
-                    <li className="texthover rightline padLR35">網路商店</li>
-                    <li className="texthover rightline padLR35">客製化商品</li>
-                    <li className="texthover rightline padLR35">會員中心</li>
-                    <li className="texthover padLR35">常見問題</li>
-                </ul>
-                <img className="h45 leanRight290"
-                     src={ICONcat}
-                     alt="iconCAR"
-                     onClick={switchPop}
-                />
-                {/* <!-- <div style="background-color: blue;">3</div> --> */}
+        <div>
+            <div className="ptabh gennavBar removeborder cenVer">
+                {/* <!-- 底線陰影 --> */}
+                <div className="fixedspace paLR20 cenVer arrHor horizontallySpaceBetween">
+                    <img className="h45" src={LOGO} alt="LOGO" />
+                    <ul className="noBullets arrHor noBreaks titleText">
+                        <li className="texthover rightline padLR35">首頁</li>
+                        <li className="texthover rightline padLR35">網路商店</li>
+                        <li className="texthover rightline padLR35">客製化商品</li>
+                        <li className="texthover rightline padLR35">會員中心</li>
+                        <li className="texthover padLR35">常見問題</li>
+                    </ul>
+                    <img className="h45 leanRight290"
+                        src={ICONcat}
+                        alt="iconCAR"
+                        onClick={switchPop}
+                    />
+                    {/* <!-- <div style="background-color: blue;">3</div> --> */}
+                </div>
             </div>
+            {popSwitch && (<PopCart onClose={switchPop} />)}
         </div>
-    </header>
-    {popSwitch && (<PopCart onClose={switchPop} />)}
-        </>
+
     );
 }
 
